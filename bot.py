5609d3ef844b93f2883a3fff4cea80926eca5167
Portfoliosuz <myportfoliosuzb@gmail.com>
2021-12-31 00:23:26 +0500

    update all

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from telebot import TeleBot, types
 import config
 import functions
-
+from time import sleep
 
 bot = TeleBot(config.token)
 
-
+pdf = ''
 @bot.message_handler(commands=['start'])
 def start(message):
     id = message.chat.id
@@ -14,45 +14,115 @@ def start(message):
          parse_mode="html")
 @bot.message_handler(content_types=["photo"])
 def photo(message):
+    global pdf
     id = message.chat.id
-    try:
-        functions.uploading_images(id,message,bot)
-    except:
-        print("Not upload")
+    functions.uploading_images(id,message,bot)
     bot.delete_message(id, message.message_id)
+
+
+
 @bot.message_handler(commands=["convert"])
 def convert(message):
     global pdf
     id = message.chat.id
     bot.delete_message(id, message.message_id)
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton(text="Pdf", callback_data="pdf"),types.InlineKeyboardButton(text="🗑", callback_data="del"))
     try:
-        pdf = bot.send_message(id, "Images: " + str(functions.get_images_count(id)), reply_markup=markup)
+        key = functions.get_key(id, bot)
+        pdf = bot.send_message(id, "Images: " + str(functions.get_images_count(id)), reply_markup=key)
     except:
         pdf = bot.send_message(id, "Not Found!")
+@bot.message_handler(content_types='text')
+def convert(message):
+    id = message.chat.id
+    if '/convert' in message.text and message.text[8:]:
+        try:
+            quality = message.text[8:]
+            bot.delete_message(id, message.message_id)
+            msg = bot.send_message(id, functions.progress(100, 0))
 
-@bot.callback_query_handler(func=lambda call:True)
-def call_(call):
-    if call.data == "pdf":
-        id = call.message.chat.id
-        bot.delete_message(id, pdf.message_id)
-        try:
-            functions.creating_pdf(directory=id, name=f"@{bot.get_me().username} {id}")
-        except:
-            bot.send_message(id, "Not Found!")
-        document = open(f"@{bot.get_me().username} {id}.pdf", "rb")
-        bot.send_document(id, document)
-        try:
+
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text=functions.progress(100, 30))
+            functions.creating_pdf(directory=id, name=f"@{bot.get_me().username} {id}",qua=quality)
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text=functions.progress(100, 60))
+
+            document = open(f"@{bot.get_me().username} {id}.pdf", "rb")
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text=functions.progress(100, 90))
+            bot.send_document(id, document)
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text=functions.progress(100, 100))
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text="✅")
+            bot.delete_message(id, msg.message_id)
+
             functions.delete_folder(id)
             functions.delete_file(f"@{bot.get_me().username} {id}.pdf")
         except:
-            bot.send_message(id, f"Not delete folder {id}")
+            return ''
+
+
+    return ''
+
+@bot.message_handler(content_types=['video', 'audio', 'voice', 'sticker'])
+def start(message):
+    id = message.chat.id
+    bot.send_message(
+        id, f"<b>Only picture, Please!</b>",
+         parse_mode="html")
+
+@bot.callback_query_handler(func=lambda call:True)
+def call_(call):
+    id = call.message.chat.id
+    if call.data == "pdf":
+        try:
+            bot.delete_message(id, call.message.message_id)
+            msg = bot.send_message(id, functions.progress(100, 0))
+            print(f"@{bot.get_me().username} {id}.pdf")
+            msg = bot.edit_message_text(chat_id=id,message_id=msg.message_id, text=functions.progress(100, 30))
+            functions.creating_pdf(directory=id, name=f"@{bot.get_me().username} {id}")
+            msg = bot.edit_message_text(chat_id=id,message_id=msg.message_id, text=functions.progress(100, 60))
+
+            document = open(f"@{bot.get_me().username} {id}.pdf", "rb")
+            msg = bot.edit_message_text(chat_id=id,message_id=msg.message_id, text=functions.progress(100, 90))
+            bot.send_document(id, document)
+            msg = bot.edit_message_text(chat_id=id,message_id=msg.message_id, text=functions.progress(100, 100))
+            msg = bot.edit_message_text(chat_id=id,message_id=msg.message_id, text="✅")
+
+            bot.delete_message(id, msg.message_id)
+            functions.delete_folder(id)
+            functions.delete_file(f"@{bot.get_me().username} {id}.pdf")
+
+            functions.delete_file(f"@{bot.get_me().username} {id}1.3.pdf")
+            functions.delete_file(f"@{bot.get_me().username} {id}1.5.pdf")
+            functions.delete_file(f"@{bot.get_me().username} {id}1.pdf")
+        except:
+            pass
+
+        
+
     if call.data == "del":
         id = call.message.chat.id
-        bot.delete_message(id, pdf.message_id)
         try:
-            functions.delete_folder(id)
+            bot.delete_message(id, pdf.message_id)
+            functions.delete_folder(str(id))
         except:
-            bot.send_message(id, f"Not delete folder {id}")
+            print(f"Not delete folder {id}")
+    if call.data[:7] == 'convert':
+        try:
+            bot.delete_message(id, pdf.message_id)
+            msg = bot.send_message(id, functions.progress(100, 0))
+            print(f"@{bot.get_me().username} {id}.pdf")
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text=functions.progress(100, 30))
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text=functions.progress(100, 60))
+    
+            document = open(f'@{bot.get_me().username} {id}{call.data[7:]}.pdf', "rb")
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text=functions.progress(100, 90))
+            bot.send_document(id, document)
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text=functions.progress(100, 100))
+            msg = bot.edit_message_text(chat_id=id, message_id=msg.message_id, text="✅")
+
+            bot.delete_message(id, msg.message_id)
+            functions.delete_folder(id)
+            functions.delete_file(f"@{bot.get_me().username} {id}1.3.pdf")
+            functions.delete_file(f"@{bot.get_me().username} {id}1.5.pdf")
+            functions.delete_file(f"@{bot.get_me().username} {id}1.pdf")
+        except:
+            print("err")
 bot.polling()
--- functions.py
+++ functions.py
@@ -1,5 +1,16 @@
 import os
 import img2pdf
+from PIL import Image
+from telebot import types
+
+def progress(lenght, width):
+          percent = width /lenght
+          percent = round(percent, 3) *100
+          done = "█" * int(percent/5) # 
+          go = "░" * (20 -int(percent/5))
+          return "|"+ done +go  +f"| [{(round(percent, 2))} %]"
+
+
 
 # create
 def create_folder(directory):
@@ -9,7 +20,7 @@ def create_folder(directory):
 # delete
 
 def delete_file(file_name):
-    os.remove(file_name)
+    os.remove(str(file_name))
 def delete_folder(directory):
     for f  in os.listdir(str(directory)):
         delete_file(str(directory)+"/" + f)
@@ -20,18 +31,32 @@ def delete_folder(directory):
 
 # get images
 
-
-def get_images(directory):
+def resize(file_path, qua):
+    print(file_path)
+    qua = float(qua)
+    file_path = str(file_path)
+    img = Image.open(file_path)
+    size = img.size
+    img = img.resize((int(size[0] / qua), int(size[1] / qua)))
+    file_path = file_path[:-4] + 'r' + '.jpg'
+    img.save(file_path)
+    return file_path
+def get_images(directory, qua=None):
     images = []
     if directory in os.listdir():
-        print("Finded")
+        print("Finded directory: ", directory)
     else:
-        return []
-    for file in os.listdir(f"{directory}/"):
-        if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
-            images.append(directory + "/" + str(file))
+        return images
+    if 'Thumbs.db' in os.listdir(directory + "/"):
+        os.remove(directory + "/Thumbs.db")
+
+    for i in  [*range(1,len(os.listdir(str(directory))) + 1)]:
+        if qua:
+            filepath = resize(f'{directory}/{i}.jpg', qua)
+            images.append(filepath)
         else:
-            continue
+            images.append(f'{directory}/{i}.jpg')
+
     return images
 def uploading_images(id, message, bot):
     id = str(id)
@@ -39,7 +64,8 @@ def uploading_images(id, message, bot):
         print("Finded")
     else:
         create_folder(id)
-
+    if 'Thumbs.db' in os.listdir(id+"/"):
+        os.remove(id+"/Thumbs.db")
     raw = message.photo[len(message.photo)-1].file_id
     path = raw + ".jpg"
     file_info = bot.get_file(raw)
@@ -52,10 +78,25 @@ def get_images_count(id):
     result = os.listdir(f"{id}/")
     result = len(result)
     return result
-
-
-def creating_pdf(directory, name=None):
-    images_list = get_images(str(directory))
+def creating_pdf(directory, name=None, qua=''):
+    images_list = get_images(str(directory),qua=qua)
+    print(images_list)
     if images_list:
-        with open(f"{name}.pdf", "wb") as f:
+        with open(f"{name}{qua}.pdf", "wb") as f:
             f.write(img2pdf.convert(images_list))
+    for i in images_list:
+        if str(i)[-5] == "r":
+            os.remove(i)
+def get_key(id, bot):
+    markup = types.InlineKeyboardMarkup()
+    for i in [1.5, 1.3, 1]:
+        creating_pdf(id, name=f"@{bot.get_me().username} {id}",qua=i)
+        file_size = os.path.getsize(f'@{bot.get_me().username} {id}{i}.pdf') / 1024
+        size_in = 'kb'
+        if file_size > 1024:
+            file_size  /= 1024
+            size_in = 'mb'
+        markup.add(types.InlineKeyboardButton(text=f"📁  {int(file_size):,} {size_in}", callback_data=f'convert{i}'))
+    markup.add(types.InlineKeyboardButton(text="Original", callback_data="pdf"),
+               types.InlineKeyboardButton(text="🗑", callback_data="del"))
+    return markup
